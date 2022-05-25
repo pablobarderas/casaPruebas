4dbb5e495060a721002b22e135251709ed7cac62
pablo <pablo@pablo-Ubuntu20>
2022-05-25 21:30:50 +0200

    Línea agregada 2

--- CasitaPrueba/src/casita/Main.java
+++ CasitaPrueba/src/casita/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Recién creado");
+		System.out.println("Línea después de ser creado el proyecto");
 	}
 
 }
