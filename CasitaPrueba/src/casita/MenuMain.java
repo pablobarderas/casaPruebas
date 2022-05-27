6ff7e78f9cb3eb21f5095bb06d5008b20073f85b
pablo <pablo@pablo-Ubuntu20>
2022-05-27 19:14:16 +0200

    Editada la clase MenuMain

--- CasitaPrueba/src/casita/MenuMain.java
+++ CasitaPrueba/src/casita/MenuMain.java
@@ -1,10 +1,25 @@
 package casita;
 
+import java.util.Scanner;
+
 public class MenuMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	public static void menu() {
+		String respuesta="";
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("----------------");
+		System.out.println("CHOOSE A OPTION");
+		System.out.println("1. ");
+		
+		switch(respuesta) {
+		case"1":
+			break;
+		case"2":
+			break;
+		}//Fin switch
+		
+		
+	}//Fin metodo
 
 }
